9f1da54d16ae31d5a66e5b9e5480d008165b0bcb
micropawns <admin@micropawns.com>
2016-05-31 08:04:33 +0700

    update

--- main.go
+++ main.go
@@ -1,55 +1,66 @@
+package rangeParser
+
+import (
+    "strings"
+    "strconv"
+    "math"
+    "fmt"
+)
+
 type Range struct {
-  start int
-  end int
+    Type string
+    Start int
+    End int
 }
 
-func rangeParser(size, str) (range *Range) {
-/*
-var index = str.indexOf('=')
+func rangeParser(size int, str string) []Range {
+    // result := Range{start: 0, end: 0}
+    index := strings.IndexAny(str, "=")
+    rangeType := string([]byte(str)[:index])
+    if index == -1 {
+        return []Range{Range{Type: rangeType, Start: 0, End: -2}}
+    }
+    // split the range string
+    arr := strings.Split(strings.Trim(str, string([]byte(str)[:(index + 1)])), ",")
+    var ranges []Range
 
-  if (index === -1) {
-    return -2
-  }
+    // parse all ranges
+    for i := 0; i < len(arr); i++ {
+        rng := strings.Split(arr[i], "-")
+        start, _ := strconv.Atoi(rng[0])
+        end, _ := strconv.Atoi(rng[1])
 
-  // split the range string
-  var arr = str.slice(index + 1).split(',')
-  var ranges = []
+        // -nnn
+        if math.IsNaN(float64(start)) == true {
+            start = size - end
+            end = size - 1
+        // nnn-
+        } else if math.IsNaN(float64(end)) == true {
+            end = size - 1
+        }
 
-  // add ranges type
-  ranges.type = str.slice(0, index)
+        // limit last-byte-pos to current length
+        if end > size - 1 {
+            end = size - 1
+        }
 
-  // parse all ranges
-  for (var i = 0; i < arr.length; i++) {
-    var range = arr[i].split('-')
-    var start = parseInt(range[0], 10)
-    var end = parseInt(range[1], 10)
+        // invalid or unsatisifiable
+        if math.IsNaN(float64(start)) || math.IsNaN(float64(end)) || start > end || start < 0 {
+            continue
+        }
 
-    // -nnn
-    if (isNaN(start)) {
-      start = size - end
-      end = size - 1
-    // nnn-
-    } else if (isNaN(end)) {
-      end = size - 1
+        // add range
+        ranges = append(ranges, Range{Type: rangeType, Start: start, End: end})
     }
 
-    // limit last-byte-pos to current length
-    if (end > size - 1) {
-      end = size - 1
+    if len(ranges) > 0 {
+        return ranges
     }
 
-    // invalid or unsatisifiable
-    if (isNaN(start) || isNaN(end) || start > end || start < 0) {
-      continue
-    }
-
-    // add range
-    ranges.push({
-      start: start,
-      end: end
-    })
-  }
-
-  return ranges.length ? ranges : -1
-*/
+    return []Range{Range{Type: rangeType, Start: 0, End: -1}}
 }
+
+// func main() {
+//     rng := rangeParser(1000, "bytes=40-80,-1")
+//     fmt.Println( rng[0].Type )
+// }
